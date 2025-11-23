2314174ee9cf188043aaf8883681ac93785b11f1
Itachi <itachijodx72@gmail.com>
2025-11-23 11:00:13 +0545

    Update index.js

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ app.get('/download', async (req, res) => {
     if (!url) return res.status(400).json({ error: 'Missing url parameter' });
 
     try {
-        // GET request to FastDL with headers mimicking browser
-        const response = await axios.get('https://fastdl.app/api/convert', {
+        // GET request to Fastok converted from POST
+        const response = await axios.get('https://api-wh.fastok.app/api/convert', {
             params: { url },
             headers: {
                 'Accept': 'application/json, text/plain, */*',
-                'Content-Type': 'application/json',
-                'Origin': 'https://fastdl.app',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'Origin': 'https://fastol.app',
                 'Referer': 'https://fastdl.app/',
                 'User-Agent': 'Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36',
                 'Sec-Ch-Ua': '"Chromium";v="137", "Not=A?Brand";v="24"',
@@ -29,7 +29,8 @@ app.get('/download', async (req, res) => {
                 'Sec-Fetch-Mode': 'cors',
                 'Sec-Fetch-Site': 'same-site',
                 'Accept-Encoding': 'gzip, deflate, br',
-                'Accept-Language': 'en-US,en;q=0.9'
+                'Accept-Language': 'en-US,en;q=0.9',
+                'Cookie': 'insert-your-cookies-here-if-needed'
             },
             timeout: 15000
         });
